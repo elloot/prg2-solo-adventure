c25f641ede304b20c1ce00e593a9dd1b60ef5d14
elloot <elliot.duchek@gmail.com>
2021-01-26 13:33:07 +0100

    add a class for the scenes of the solo adventure, complete with JavaDoc comments

---
+++ src/Scene.java
@@ -0,0 +1,44 @@
+/**
+ * A Model class for the scenes in this solo adventure. A Scene contains an id and a body. The body holds part of the
+ * story of the solo adventure and tells the player what happens in this scene.
+ */
+
+public class Scene {
+    private int id;
+    private String body;
+
+    /**
+     * Constructs a scene with a body.
+     * @param body A String describing what part of the solo adventure's story the player is currently in.
+     */
+    public Scene(String body) {
+        this.body = body;
+    }
+
+    /**
+     * Constructs a scene with an id and a body.
+     * @param id The id of the scene.
+     * @param body A String describing what part of the solo adventure's story the player is currently in.
+     */
+    public Scene(int id, String body) {
+        this.id = id;
+        this.body = body;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
