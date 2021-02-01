1907110eebedf98451f1b784ee3b6a8e8198054a
elloot <elliot.duchek@gmail.com>
2021-02-01 11:13:28 +0100

    add getters and JavaDoc description

--- src/StoryGUI.java
+++ src/StoryGUI.java
@@ -1,8 +1,25 @@
 import javax.swing.*;
 
+/**
+ * A View class to show the current scene and options to the player of the solo adventure. A StoryGUI has a text
+ * panel for showing the story body, a dropdown menu to show the current options available to the player and a button
+ * for the player to confirm their choice.
+ */
 public class StoryGUI {
     private JPanel panel;
     private JTextPane sceneBody;
     private JComboBox<Scene> sceneSelector;
     private JButton confirmSelection;
+
+    public JTextPane getSceneBody() {
+        return sceneBody;
+    }
+
+    public JComboBox<Scene> getSceneSelector() {
+        return sceneSelector;
+    }
+
+    public JButton getConfirmSelection() {
+        return confirmSelection;
+    }
 }
