809a443564c4cb596e03174de139353a060b9242
elloot <elliot.duchek@gmail.com>
2021-02-01 10:31:02 +0100

    add scene selector and confirm button

--- src/StoryGUI.java
+++ src/StoryGUI.java
@@ -3,4 +3,6 @@ import javax.swing.*;
 public class StoryGUI {
     private JPanel panel;
     private JTextPane sceneBody;
+    private JComboBox<Scene> sceneSelector;
+    private JButton confirmSelection;
 }
