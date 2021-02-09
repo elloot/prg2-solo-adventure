eaee7ca8b4c6c163a6d67470ba07acc060be33f9
elloot <elliot.duchek@gmail.com>
2021-02-09 13:37:56 +0100

    start working on the GUI for the editor

---
+++ src/EditorGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class EditorGUI {
+    private JPanel panel;
+    private JTextPane textPane1;
+    private JComboBox comboBox1;
+    private JTextPane textPane2;
+    private JComboBox comboBox2;
+    private JTextPane textPane3;
+    private JTextPane textPane4;
+    private JButton button1;
+}
