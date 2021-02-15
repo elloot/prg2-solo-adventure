0d991025327c05079f92a5f6132d72e8ce5214a5
elloot <elliot.duchek@gmail.com>
2021-02-15 10:52:18 +0100

    add GUI for creation of links, can select target scene and edit link description

---
+++ src/LinkCreationGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class LinkCreationGUI {
+    private JPanel panel;
+    private JTextPane targetSceneBody;
+    private JTextPane linkDescription;
+    private JTextPane sourceSceneBody;
+    private JComboBox targetSceneSelector;
+    private JPanel textFeildPanel;
+    private JButton addLinkButton;
+}
