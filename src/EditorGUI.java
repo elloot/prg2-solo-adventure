aa7dcec5e52aade6176331c95ad66ba431a58aac
elloot <elliot.duchek@gmail.com>
2021-02-15 10:51:38 +0100

    redesign editor GUI completely, now displays story and link description with buttons to remove, edit and add

--- src/EditorGUI.java
+++ src/EditorGUI.java
@@ -2,11 +2,16 @@ import javax.swing.*;
 
 public class EditorGUI {
     private JPanel panel;
-    private JTextPane textPane1;
-    private JComboBox comboBox1;
-    private JTextPane textPane2;
-    private JComboBox comboBox2;
-    private JTextPane textPane3;
-    private JTextPane textPane4;
-    private JButton button1;
+    private JComboBox sceneSelector;
+    private JTextPane sceneBody;
+    private JButton addSceneButton;
+    private JButton editSceneButton;
+    private JComboBox linkSelector;
+    private JTextPane linkDescription;
+    private JPanel sceneButtonsPanel;
+    private JButton addLinkButton;
+    private JButton editLinkButton;
+    private JPanel linkButtonsPanel;
+    private JButton removeSceneButton;
+    private JButton removeLinkButton;
 }
